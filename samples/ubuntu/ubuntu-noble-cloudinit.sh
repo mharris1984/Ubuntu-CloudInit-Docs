7bcae51398063b13571ed082729a84b56be904e0
Mike Harris <mharris1984@gmail.com>
2025-08-03 15:54:24 -0700

    Update ubuntu-noble-cloudinit.sh

--- samples/ubuntu/ubuntu-noble-cloudinit.sh
+++ samples/ubuntu/ubuntu-noble-cloudinit.sh
@@ -2,20 +2,22 @@
 
 VMID=8200
 STORAGE=NetAppPool3
+USER=adminmike
 
 set -x
-rm -f noble-server-cloudimg-amd64.img
-wget -q https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
-qemu-img resize noble-server-cloudimg-amd64.img 8G
-sudo qm destroy $VMID
+rm -f ubuntu-24.04-minimal-cloudimg-amd64.img
+wget -q https://cloud-images.ubuntu.com/minimal/releases/noble/release/ubuntu-24.04-minimal-cloudimg-amd64.img
+mv ubuntu-24.04-minimal-cloudimg-amd64.img ubuntu-22.04.qcow2
+qemu-img resizeubuntu-22.04.qcow2 120G
+sudo qm destroy $VMID11
 sudo qm create $VMID --name "ubuntu-noble-template" --ostype l26 \
-    --memory 1024 --balloon 0 \
+    --memory 4092 --balloon 0 \
     --agent 1 \
     --bios ovmf --machine q35 --efidisk0 $STORAGE:0,pre-enrolled-keys=0 \
     --cpu host --socket 1 --cores 1 \
     --vga serial0 --serial0 socket  \
     --net0 virtio,bridge=vmbr0
-sudo qm importdisk $VMID noble-server-cloudimg-amd64.img $STORAGE
+sudo qm importdisk $VMID ubuntu-22.04.qcow2 $STORAGE
 sudo qm set $VMID --serial0 socket --vga serial0
 sudo qm set $VMID --scsihw virtio-scsi-pci --virtio0 $STORAGE:vm-$VMID-disk-1,discard=on
 sudo qm set $VMID --boot order=virtio0
@@ -31,7 +33,7 @@ runcmd:
 # Taken from https://forum.proxmox.com/threads/combining-custom-cloud-init-with-auto-generated.59008/page-3#post-428772
 EOF
 sudo qm set $VMID --cicustom "vendor=local:snippets/ubuntu.yaml"
-sudo qm set $VMID --tags ubuntu-template,noble,cloudinit
+sudo qm set $VMID --tags ubuntu-24.04,ubuntu-template,noble,cloudinit
 sudo qm set $VMID --ciuser $USER
 sudo qm set $VMID --sshkeys ~/.ssh/authorized_keys
 sudo qm set $VMID --ipconfig0 ip=dhcp
